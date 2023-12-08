044276786ad0b30014116437d6b0fc8977924537
Praveen Kumar <108298974+Praveen-75@users.noreply.github.com>
2023-12-08 10:52:56 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,19 @@ mongoose.connect("mongodb://localhost/login")
 
 const userSchema = mongoose.Schema({
   name: String,
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3,
+  },
   email: String,
   mobile: Number,
   password: String,
+  setReminder: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "add"
+  }]
 })
-mongoose.plugin(plm)
+mongoose.plugin(plm);
 module.exports = mongoose.model("rem", userSchema);
